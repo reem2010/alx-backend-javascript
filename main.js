48b88b1d3ee509036da3be32617a4d8e8cd90463
reem <tarekreem950@gmail.com>
2024-03-11 01:38:18 +0200

    Iterator

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-import appendToEachArrayValue from './0x00-ES6_basic/10-loops.js';
+import createEmployeesObject from './0x00-ES6_basic/11-createEmployeesObject';
 
-console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
+console.log(createEmployeesObject("Software", [ "Bob", "Sylvie" ]));
