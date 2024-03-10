a47fba71bc04fc7efede74df9185c2e604ca1d19
reem <tarekreem950@gmail.com>
2024-03-11 01:32:53 +0200

    loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,7 +2,8 @@ export default function appendToEachArrayValue(array, appendString) {
   let i = 0;
   for (const val of array) {
     const value = val;
-    array[i++] = appendString + value;
+    array[i] = appendString + value; // eslint-disable-line no-param-reassign
+    i++; // eslint-disable-line no-plusplus
   }
 
   return array;
