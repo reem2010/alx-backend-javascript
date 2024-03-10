2eace74e5c619f3610ec0c1784b639562b314d9e
reem <tarekreem950@gmail.com>
2024-03-10 23:56:53 +0200

    spread

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import returnHowManyArguments from './0x00-ES6_basic/4-rest-parameter.js';
+import concatArrays from './0x00-ES6_basic/5-spread-operator';
 
-
-console.log(returnHowManyArguments("one"));
-console.log(returnHowManyArguments("one", "two", 3, "4th"));
+console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
