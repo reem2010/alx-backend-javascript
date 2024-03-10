3650a694f2b7531b3e30bf5531ccb962d290cf66
reem <tarekreem950@gmail.com>
2024-03-11 01:16:10 +0200

    method properties

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -2,9 +2,10 @@ import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = { ...budget };
-  fullBudget.getIncomeInDollars = (income) => `$${income}`;
-  fullBudget.getIncomeInEuros = (income) => `${income} euros`;
-
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) { return `$${income}`; },
+    getIncomeInEuros(income) { return `${income} euros`; },
+  };
   return fullBudget;
 }
