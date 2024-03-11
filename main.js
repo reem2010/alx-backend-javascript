ea35c02c7cadcd1221d8894265fc9a3279442d37
reem <tarekreem950@gmail.com>
2024-03-11 02:22:11 +0200

    create string

--- main.js
+++ main.js
@@ -3,15 +3,14 @@ import createEmployeesObject from './0x00-ES6_basic/11-createEmployeesObject';
 import createReportObject from './0x00-ES6_basic/12-createReportObject';
 
 import createIteratorObject from "./0x00-ES6_basic/100-createIteratorObject";
+import iterateThroughObject from './0x00-ES6_basic/101-iterateThroughObject';
+
 const employees = {
     ...createEmployeesObject('engineering', ['Bob', 'Jane']),
     ...createEmployeesObject('marketing', ['Sylvie'])
 };
 
 const report = createReportObject(employees);
-
 const reportWithIterator = createIteratorObject(report);
 
-for (const item of reportWithIterator) {
-    console.log(item);
-}
+console.log(iterateThroughObject(reportWithIterator));
