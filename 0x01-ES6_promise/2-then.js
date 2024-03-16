202a06aa97af0e9d75035d370fff7cde4e77b28e
reem <tarekreem950@gmail.com>
2024-03-17 01:35:07 +0200

    wait for a promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,5 +3,5 @@ export default function handleResponseFromAPI(promise) {
     () => ({ status: 200, body: 'success' }),
     () => new Error(),
     console.log('Got a response from the API'),
-  )
+  );
 }
