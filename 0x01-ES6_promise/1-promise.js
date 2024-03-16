c1a93232df8158e92479404369f2b414feecec84
reem <tarekreem950@gmail.com>
2024-03-17 00:54:24 +0200

    promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise(((res, rej) => {
+    if (success) {
+      res({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      rej(new Error('The fake API is not working currently'));
+    }
+  }));
+  return promise;
+}
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import getResponseFromAPI from "./0x01-ES6_promise/0-promise.js";
+import getFullResponseFromAPI from './0x01-ES6_promise/1-promise.js';
 
-const response = getResponseFromAPI();
-console.log(response instanceof Promise);
+console.log(getFullResponseFromAPI(true));
+console.log(getFullResponseFromAPI(false));
