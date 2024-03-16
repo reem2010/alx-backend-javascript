2a17bd410cbef182c43160b9ef731104f5e2c456
reem <tarekreem950@gmail.com>
2024-03-17 00:41:52 +0200

    newpromise

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-import createEmployeesObject from './0x00-ES6_basic/11-createEmployeesObject';
+import getResponseFromAPI from "./0x01-ES6_promise/0-promise.js";
 
-import createReportObject from './0x00-ES6_basic/12-createReportObject';
-
-import createIteratorObject from "./0x00-ES6_basic/100-createIteratorObject";
-import iterateThroughObject from './0x00-ES6_basic/101-iterateThroughObject';
-
-const employees = {
-    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
-    ...createEmployeesObject('marketing', ['Sylvie'])
-};
-
-const report = createReportObject(employees);
-const reportWithIterator = createIteratorObject(report);
-
-console.log(iterateThroughObject(reportWithIterator));
+const response = getResponseFromAPI();
+console.log(response instanceof Promise);
