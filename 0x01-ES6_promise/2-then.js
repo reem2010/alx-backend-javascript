d83e2fbd0934cd4aeabe09e746991e8be2b20d77
reem <tarekreem950@gmail.com>
2024-03-17 01:24:19 +0200

    wait for a promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(
-    () => ({
+    () => (Promise.resolve({
       status: 200,
       body: 'success',
-    }),
-    () => new Error(),
+    })),
+    () => Promise.reject(new Error()),
     console.log('Got a response from the API'),
   );
 }
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import handleResponseFromAPI from "./0x01-ES6_promise/2-then.js";
 
-const promise = Promise.reject();
+const promise = Promise.resolve();
 handleResponseFromAPI(promise);
