2dbb2197c3bd04c49b8af9b67014c35dfb59b4c6
reem <tarekreem950@gmail.com>
2024-03-18 01:31:59 +0200

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((val) => {
-    createUser().then((value) => console.log(val.body, value.firstName, value.lastName));
-  });
+  return uploadPhoto().then((val) => {
+    createUser().then((value) => console.log(val.body, value.firstName, value.lastName)).catch(console.log('Signup system offline'));
+  }).catch(console.log('Signup system offline'));
 }
