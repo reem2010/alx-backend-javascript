18eec7d73b812bc5582279135e7bbe5ec888188d
reem <tarekreem950@gmail.com>
2024-04-06 21:10:15 +0200

    all settled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,10 @@ import signUp from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUp(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([signUp(firstName, lastName), uploadPhoto(fileName)]).then(
+    (val) => val.map((p) => ({
+      status: p.status,
+      value: p.value || String(p.reason),
+    })),
+  );
 }
--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 import handleProfileSignup from './0x01-ES6_promise/6-final-user';
 
-console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg").then((val)=> console.log(val)));
