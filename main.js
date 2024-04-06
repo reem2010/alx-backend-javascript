da259901af6a8d686b04d872ebdb6b1e42c4337e
reem <tarekreem950@gmail.com>
2024-04-06 20:07:57 +0200

    reject promise

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-import signUpUser from "./0x01-ES6_promise/4-user-promise.js";
+import uploadPhoto from './0x01-ES6_promise/5-photo-reject.js';
 
-console.log(signUpUser("Bob", "Dylan"));
+console.log(uploadPhoto('guillaume.jpg'));
