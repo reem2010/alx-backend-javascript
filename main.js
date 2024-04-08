d8641cd3450b7db679412e74094d37ce1cdace72
reem <tarekreem950@gmail.com>
2024-04-08 02:13:33 +0200

    task 9

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-import HolbertonClass from "./0x02-ES6_classes/8-hbtn_class";
+import listOfStudents from "./0x02-ES6_classes/9-hoisting";
 
-const hc = new HolbertonClass(12, "Mezzanine")
-console.log(Number(hc));
-console.log(String(hc));
+console.log(listOfStudents);
+
+const listPrinted = listOfStudents.map(
+    student => student.fullStudentDescription
+);
+
+console.log(listPrinted)
