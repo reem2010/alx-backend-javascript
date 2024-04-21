907eb87366c819a45a2049233abb0a8c9a7290a2
reem <tarekreem950@gmail.com>
2024-04-22 01:41:11 +0200

    task2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 function getStudentsByLocation(students, city) {
-  return students.map((data) => data.location === city);
+  return students.filter((data) => data.location === city);
 }
 
 export default getStudentsByLocation;
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import getListStudentIds from "./0x03-ES6_data_manipulation/1-get_list_student_ids.js";
+import getStudentsByLocation from "./0x03-ES6_data_manipulation/2-get_students_by_loc.js";
 import getListStudents from "./0x03-ES6_data_manipulation/0-get_list_students.js";
+const students = getListStudents();
 
-console.log(getListStudentIds("hello"));
-console.log(getListStudentIds(getListStudents()));
+console.log(getStudentsByLocation(students, 'San Francisco'));
