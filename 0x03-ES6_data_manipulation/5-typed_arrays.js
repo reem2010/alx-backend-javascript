322270956853540ba8b9c3e0da7eb76c39d6ac4c
reem <tarekreem950@gmail.com>
2024-04-22 02:23:23 +0200

    task5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+function createInt8TypedArray(length, position, value) {
+  const buf = new ArrayBuffer(length);
+  const int8 = new Int8Array(buf);
+  if (position > int8.length - 1) {
+    throw Error('Position outside range');
+  }
+  int8[position] = value;
+  return buf;
+}
+
+export default createInt8TypedArray;
--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-import updateStudentGradeByCity from "./0x03-ES6_data_manipulation/4-update_grade_by_city.js";
-import getListStudents from "./0x03-ES6_data_manipulation/0-get_list_students.js";
+import createInt8TypedArray from "./0x03-ES6_data_manipulation/5-typed_arrays";
 
-
-console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
-
-console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }]));
-
+console.log(createInt8TypedArray(10, 2, 89))
