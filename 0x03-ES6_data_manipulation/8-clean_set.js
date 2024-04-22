a68f5615b5819a8acaafb5d80467adc2a5b41b17
reem <tarekreem950@gmail.com>
2024-04-22 16:00:35 +0200

    task 9

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,17 @@
 function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
   const ind = startString.length;
   const res = '';
   if (ind === 0) {
     return res;
   }
-  const array = Array.from(set).filter((ele) => ele.startsWith(startString)).map((ele) => ele.replace(startString, ''));
+  const array = Array.from(set).filter(
+    (ele) => ele.startsWith(startString),
+  ).map(
+    (ele) => ele.slice(ind),
+  );
   return array.join('-');
 }
 
