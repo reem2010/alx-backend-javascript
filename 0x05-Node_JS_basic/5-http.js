f39d5ba0c1a347df826f41aeac6a3a3834d04525
reem <tarekreem950@gmail.com>
2024-04-30 21:57:02 +0300

    task5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -11,28 +11,32 @@ function countStudents(url, responce) {
       }
       let data = data1.split('\n').slice(1);
       data = data.filter((val) => val);
-      responce.write(`Number of students: ${data.length}\n`);
-      data = data.map((val) => val.split(','));
-      data = data.map((val) => val.filter(
-        (_, ind, arr) => ind === 0 || ind === arr.length - 1,
-      ))
-        .map((val) => ({ name: val[0], FIELD: val[1] }));
-      data.forEach((ele) => {
-        if (!res[ele.FIELD]) res[ele.FIELD] = [];
-        res[ele.FIELD] = [...res[ele.FIELD], ele.name];
-      });
-      const key = Object.keys(res);
-      const key1 = key.slice(0, key.length - 1);
-      key1.forEach((ele) => {
-        responce.write(
-          `Number of students in ${ele}:`
+      if (data.length === 0) responce.write(`Number of students: ${data.length}`);
+      else {
+        responce.write(`Number of students: ${data.length}\n`);
+        data = data.map((val) => val.split(','));
+        data = data.map((val) => val.filter(
+          (_, ind, arr) => ind === 0 || ind === arr.length - 1,
+        ))
+          .map((val) => ({ name: val[0], FIELD: val[1] }));
+        data.forEach((ele) => {
+          if (!res[ele.FIELD]) res[ele.FIELD] = [];
+          res[ele.FIELD] = [...res[ele.FIELD], ele.name];
+        });
+        const key = Object.keys(res);
+        const key1 = key.slice(0, key.length - 1);
+        key1.forEach((ele) => {
+          responce.write(
+            `Number of students in ${ele}:`
       + ` ${res[ele].length}. List: ${res[ele].join(', ')}\n`,
-        );
-      });
-      responce.write(
-        `Number of students in ${key[key.length - 1]}:`
+          );
+        });
+
+        responce.write(
+          `Number of students in ${key[key.length - 1]}:`
     + ` ${res[key[key.length - 1]].length}. List: ${res[key[key.length - 1]].join(', ')}`,
-      );
+        );
+      }
       resp();
     });
   });
