8b885c5b14c506f9b76176d70073dc5fea969ea7
reem <tarekreem950@gmail.com>
2024-04-30 20:32:09 +0300

    task2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function countStudents(url) {
+  let data = '';
+  const res = {};
+  try {
+    data = fs.readFileSync(url, 'utf8');
+  } catch (err) {
+    throw Error('Cannot load the database');
+  }
+  data = data.split('\n').slice(1);
+
+  console.log(`Number of students: ${data.length}`);
+  data = data.map((val) => val.split(','));
+  data = data.map((val) => val.filter(
+    (_, ind, arr) => ind === 0 || ind === arr.length - 1,
+  ))
+    .map((val) => ({ name: val[0], FIELD: val[1] }));
+  data.forEach((ele) => {
+    if (!res[ele.FIELD]) res[ele.FIELD] = [];
+    res[ele.FIELD] = [...res[ele.FIELD], ele.name];
+  });
+  const key = Object.keys(res);
+  key.forEach((ele) => {
+    console.log(
+      `Number of students in ${ele}:`
+      + ` ${res[ele].length}. List: ${res[ele].join(', ')}`,
+    );
+  });
+}
+countStudents('database.csv');
