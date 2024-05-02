fd77d1dca1375c45369764b2260aae38a518632f
reem <tarekreem950@gmail.com>
2024-05-02 18:52:50 +0300

    task8

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,7 +2,7 @@ const calculateNumber = (type, a, b) => {
     if(type === "SUM")
         return (Math.round(a) + Math.round(b));
     else if(type === "SUBTRACT")
-        return (Math.round(a) + Math.round(b));
+        return (Math.round(a) - Math.round(b));
     else if(type === "DIVIDE"){
         if (b === 0)
             return "Error"
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('rounded sum test', function() {
+  it('first', function(){
+    assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+  });
+  it('sec', function(){
+    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  });
+  it('third', function(){
+    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+  it('fourth', function(){
+    assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  });
+});
