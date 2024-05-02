1f429ca8c6f70e18e88f05edbcca55460ba18648
reem <tarekreem950@gmail.com>
2024-05-02 21:16:02 +0300

    task3

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,12 @@
+const expect = require("chai").expect;
+const sendPaymentRequestToApi = require("./3-payment");
+const sinon = require("sinon")
+const Utils = require("utilS")
+
+describe('sendPaymentRequestToApi', function() {
+  it('spy on calculate num', function(){
+    const spy = sinon.spy(Utils, "calculateNumber")
+    sendPaymentRequestToApi(100, 20)
+    sinon.assert.calledOnce(spy)
+  });
+});
