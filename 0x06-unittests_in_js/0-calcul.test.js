437de3f0cb28a10fde86156bda6670531a0b1137
reem <tarekreem950@gmail.com>
2024-05-02 18:33:21 +0300

    task0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,15 +3,15 @@ const calculateNumber = require('./0-calcul');
 
 describe('rounded sum test', () => {
   it('first', () => {
-    assert.equal(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
   it('sec', () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
   it('third', () => {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
   it('fourth', () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
