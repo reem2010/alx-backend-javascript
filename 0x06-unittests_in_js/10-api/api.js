c5cad39d4e306cf367768618ecf7c5551104c8f6
reem <tarekreem950@gmail.com>
2024-05-04 02:24:16 +0300

    task9

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -12,6 +12,19 @@ app.get('/cart/:id(\\d+)', (req, res) => {
     res.send(`payment methods for cart ${id}`)
 })
 
+app.get('/available_payments', (req, res) => {
+    res.send({
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
+      })
+})
+
+app.post('/login', (req, res) => {
+    const name = req.body.userName
+    res.send(`Welcome ${name}`)
+})
 app.listen(7865, () => {
     console.log('API available on localhost port 7865')
 })
