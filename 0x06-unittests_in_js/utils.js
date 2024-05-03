520b22d675b6105d72817bbe008d385e773b7d60
reem <tarekreem950@gmail.com>
2024-05-03 08:26:04 +0300

    fix

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -5,7 +5,7 @@ const Utils = {
         else if(type === "SUBTRACT")
             return (Math.round(a) - Math.round(b));
         else if(type === "DIVIDE"){
-            if (b === 0)
+            if (Math.round(b) === 0)
                 return "Error"
             return (Math.round(a) / Math.round(b));
         }
