a1f97d914659d34017d19a7ab672bbc24981d5d0
reem <tarekreem950@gmail.com>
2024-05-03 08:22:19 +0300

    fix

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,7 +2,7 @@ const calculateNumber = (type, a, b) => {
   if (type === 'SUM') return (Math.round(a) + Math.round(b));
   if (type === 'SUBTRACT') return (Math.round(a) - Math.round(b));
   if (type === 'DIVIDE') {
-    if (b === 0) return 'Error';
+    if (Math.round(b) === 0) return 'Error';
     return (Math.round(a) / Math.round(b));
   }
 };
